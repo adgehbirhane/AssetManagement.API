b01708e6a82d5b02dd6c5f8db2caab28068cf0ff
Belay Birhanu Haile <adgehbirhane@gmail.com>
2025-08-10 02:28:53 +0300

    userDto updated so it will include the newly inserted profileImage atribute

--- AssetManagement.API/DTOs/AuthDTOs.cs
+++ AssetManagement.API/DTOs/AuthDTOs.cs
@@ -44,6 +44,7 @@ public class UserDto
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
+    public string? ProfileImageUrl { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
